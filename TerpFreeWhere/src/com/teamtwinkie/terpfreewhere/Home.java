bd909dfa7460d40ff3e2d34a5e7dd58de9b6fc42
HERO <mhero18@gmail.com>
2013-09-07 17:19:23 -0400

    hi

--- TerpFreeWhere/src/com/teamtwinkie/terpfreewhere/Home.java
+++ TerpFreeWhere/src/com/teamtwinkie/terpfreewhere/Home.java
@@ -1,23 +1,113 @@
 package com.teamtwinkie.terpfreewhere;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import 
 
-public class Home extends Activity {
+android.app.Activity;
+import android.view.Menu;
+import 
+
+android.view.View;
+import android.view.View.OnClickListener;
+import 
+
+android.widget.Button;
+
+public class Home extends Activity implements 
+
+OnClickListener {
+	Button freeFoodButton;
+	Button freeSwagButton; 
+	Button freeEventsButton ;
+	Button allFreeThingsButton ;
+	
+
+Button organizationButton ;
+	
+	
+	@Override
+	
+
+protected void onCreate(Bundle savedInstanceState) {
+		
+
+freeFoodButton = (Button) findViewById(R.id.button1);
+		
+
+freeSwagButton = (Button) findViewById(R.id.button2);
+		
+
+freeEventsButton = (Button) findViewById(R.id.button3);
+		
+
+allFreeThingsButton = (Button) findViewById(R.id.button4);
+		
+
+organizationButton = (Button) findViewById(R.id.button5);
+		
+
+freeFoodButton.setOnClickListener(this);
+		
+
+freeSwagButton.setOnClickListener(this);
+		
+
+freeEventsButton.setOnClickListener(this);
+		
+
+allFreeThingsButton.setOnClickListener(this);
+		
+
+super.onCreate(savedInstanceState);
+		setContentView
+
+(R.layout.activity_home);
+	}
+	
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+	public 
+
+boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		
+
+getMenuInflater().inflate(R.menu.home, menu);
+		//Comment here
+	
+
+	return true;
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
-		//Comment here
-		return true;
+	public void onClick
+
+(View v) {
+		switch(v.getId()){
+		case 
+
+R.id.button1:
+			//switch to the menu activity
+		
+
+case R.id.button2:
+			//switch to the menu activity
+	
+
+	case R.id.button3:
+			// switch to the menu activity
+		case R.id.button4:
+			
+
+//switch to the menu activity
+		case R.id.button5:
+		
+
+	//switch to the orginaztion submission activity
+		}
+	
+
+	
 	}
 
 }
