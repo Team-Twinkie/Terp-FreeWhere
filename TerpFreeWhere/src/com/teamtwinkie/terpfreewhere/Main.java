76bf0f79404c2ff3b0018bfea2c3a5c7964491f4
sdp-23 <sdp-23@live.com>
2013-09-07 20:44:19 -0400

    Added the add methods

--- TerpFreeWhere/src/com/teamtwinkie/terpfreewhere/Main.java
+++ TerpFreeWhere/src/com/teamtwinkie/terpfreewhere/Main.java
@@ -23,19 +23,44 @@ public class Main {
 		
 		categories = new HashMap<type,PriorityQueue<Entry>>();
 		
-		food = new PriorityQueue<Entry>(10, new DateComparator());
-		swag = new PriorityQueue<Entry>(10, new DateComparator());
-		activities = new PriorityQueue<Entry>(10, new DateComparator());
+		Comparator<Entry> comparator = new DateComparator();
 		
+		food = new PriorityQueue<Entry>(0, comparator);
+		swag = new PriorityQueue<Entry>(0, comparator);
+		activities = new PriorityQueue<Entry>(0, comparator);
+				
+		all = new TreeSet<Entry>(comparator);
 		
 		categories.put(type.FOOD, food);
 		categories.put(type.SWAG, swag);
 		categories.put(type.EVENTS, activities);
-		
-		all = new TreeSet<Entry>();
 	}
 	
 	/*
-	 * Adds the 
+	 * Generic Add Food
 	 */
+	void addFood(Food f1){
+		food.add(f1);
+	}
+	
+	/*
+	 * Generic Add Swag
+	 */
+	void addSwag(Swag s1){
+		swag.add(s1);
+	}
+	
+	/*
+	 * Generic Add Activities
+	 */
+	void addActivities(Events a1){
+		activities.add(a1);
+	}
+	
+	/*
+	 * Generic Add to all
+	 */
+	void addFood(Entry e1){
+		all.add(e1);
+	}
 }
