a51386ef35c6acda889620cee13abda07f9c2c62
rwolniak <wolnicat@gmail.com>
2013-09-07 22:01:15 -0400

    Added comment to DateComparator

--- TerpFreeWhere/src/com/teamtwinkie/terpfreewhere/DateComparator.java
+++ TerpFreeWhere/src/com/teamtwinkie/terpfreewhere/DateComparator.java
@@ -1,6 +1,7 @@
 package com.teamtwinkie.terpfreewhere;
 import java.util.Comparator;
 
+/*Compares two dates.*/
 public class DateComparator implements Comparator<Entry>{
 	public int compare(Entry x, Entry y) {          
         return x.getWhen().getTime().compareTo(y.getWhen().getTime());
